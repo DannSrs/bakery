fae3e76c3c8388fae4bcc55b848a2d202bc4067e
DannSrs <danniels2801@gmail.com>
2026-09-23 13:35:48 -0300

    refactor: atualiza Main para realizar pedidos complexos com Decorator

--- Main.java
+++ Main.java
@@ -3,12 +3,30 @@
  */
 public class Main {
     public static void main(String[] args) {
-        // Create the order
+        // Cria a ordem
         Order order = new Order();
-        order.addCake(new VanillaCake());
+
+        // 1. Bolo de chocolate puro
         order.addCake(new ChocolateCake());
 
-        // Print the order
+        // 2. Bolo de baunilha com o dizer "PLAIN!"
+        order.addCake(new Saying(new VanillaCake(), "PLAIN!"));
+
+        // 3. Bolo de baunilha com granulado e dizendo "FANCY!"
+        order.addCake(new Saying(new Sprinkles(new VanillaCake()), "FANCY!"));
+
+        // 4. Bolo complexo de morango!
+        // Podemos fazer instanciando passo a passo numa variável para ficar mais claro:
+        Cake complexCake = new StrawberryCake();
+        complexCake = new MultiLayered(complexCake);
+        complexCake = new Sprinkles(complexCake); // 1º granulado
+        complexCake = new Sprinkles(complexCake); // 2º granulado
+        complexCake = new Saying(complexCake, "One of");
+        complexCake = new Saying(complexCake, "EVERYTHING");
+
+        order.addCake(complexCake);
+
+        // Imprime a ordem na tela
         order.printOrder();
     }
 }
